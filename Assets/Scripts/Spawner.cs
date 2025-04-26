cc2e992dd926aaa8a87ee6c4dc2d11992cf3c63a
Hugo Gonzalez <player.hugo.gonzalez@gmail.com>
2025-04-26 12:27:06 -0600

    Add TMPro

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,9 +1,12 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour {
 	#region Public references
 	[SerializeField] private GameObject enemyPrefab;
+	[SerializeField] private TextMeshProUGUI wavesText
+;
     #endregion
 
     void Start() {
@@ -11,10 +14,20 @@ public class Spawner : MonoBehaviour {
     }
 
 	private IEnumerator SpawnEnemy(){
+		
+		for(int i=0; i<3; i++){ //Niveles
+			
+			for(int j=0; j<3; j++){//Oleadas
 
-		for(int i=0; i<5; i++){
-			Instantiate(enemyPrefab, transform.position, Quaternion.identity);
-			yield return new WaitForSeconds(1f);
+				for(int k=0; k<10; k++){//Enemigos
+					Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+					yield return new WaitForSeconds(0.5f);
+				}
+
+				yield return new WaitForSeconds(2f);
+			}
+
+			yield return new WaitForSeconds(3f);
 		}
 	}
 }
